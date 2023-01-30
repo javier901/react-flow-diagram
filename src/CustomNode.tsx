35eef6c4a9ee76d508a94ae51c7cd41e023437e7
codegenius1017 <espinoza.michael1017@gmail.com>
2023-01-30 01:58:51 -0400

    Connect nodes

--- src/CustomNode.tsx
+++ src/CustomNode.tsx
@@ -4,23 +4,27 @@ import { IPanel } from "./types";
 
 interface CustomNodeProps {
   data: IPanel;
-  isConnectable: boolean;
 }
 
-export const CustomNode = memo(({ data, isConnectable }: CustomNodeProps) => {
+export const CustomNode = memo(({ data }: CustomNodeProps) => {
   return (
     <>
       <Handle
         type="target"
         position={Position.Left}
         style={{ background: "#555" }}
-        onConnect={(params) => console.log("handle onConnect", params)}
-        isConnectable={isConnectable}
+        isConnectable={true}
       />
       <div className="border-2 border-gray-500 p-2 rounded-sm">
         <p className="border-b-2">{data.title ?? "Title not available"}</p>
         <p className="text-sm">{data.subTitle ?? "Subtitle not available"}</p>
       </div>
+      <Handle
+        type="source"
+        position={Position.Right}
+        style={{ background: "red" }}
+        isConnectable={true}
+      />
     </>
   );
 });
